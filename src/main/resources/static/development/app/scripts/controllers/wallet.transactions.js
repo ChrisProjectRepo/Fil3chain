bf408ad6842b97ed65f57fb46712ed7a414c308b
ivan18 <ivan18@localhost.localdomain>
2016-07-14 01:50:23 +0200

    Update Wallet.transactions.html

--- src/main/resources/static/development/app/scripts/controllers/wallet.transactions.js
+++ src/main/resources/static/development/app/scripts/controllers/wallet.transactions.js
@@ -9,36 +9,40 @@
 */
 angular.module('blockchainApp')
 .controller('walletTransactionCtrl',['$scope','$mdToast','transactions','transactionHeaders', function($scope, $mdToast, transactions, transactionHeaders){
-  console.log('walletTransactionCtrl',transactions,transactionHeaders);
-	
+  //console.log('walletTransactionCtrl',transactions,transactionHeaders);
+  //Oggetto atto alla memorizzazione degli indici delle transazioni le cui citazioni sono aperte
+  var openedCitations ={};
+  //questa funzione si occupa di gestire i flag di apertura del campo citations//delle varie transazioni
+  $scope.toggleCitation = function(index, transaction){
+      //console.log('Toggle transaction',index, transaction);
+      openedCitations[transaction.hashTransBlock] =! openedCitations[transaction.hashTransBlock];
+      //console.log('Toggle transaction','result', openedCitations);
+  }
+  $scope.openedCitations = openedCitations;
+  //Caso in cui non ricevo i service che mi aspetto
   if(!transactions || !transactionHeaders){
-	  console.log('TRANSACTIONS NULL');
 	  $mdToast.show(
 		        $mdToast.simple()
 		        .textContent('No transaction found')
 		        .position('bottom')
-		        //.position($scope.getToastPosition())
 		        .hideDelay(5000)
 		      );
   }else{
 	  $scope.transactions = transactions;
 	  $scope.headers = transactionHeaders;
   }
-
+  //Disabilitata la modalità selezione
   $scope.selectionMode = false;
-  $scope.addTransaction=function(transaction){
-	  console.log('walletTransactionCtrl','Transaction click',transaction)
-  }
- 
 
 
-    $scope.selectedUserIndex = undefined;
+
+    $scope.selectedTransactionIndex = undefined;
     $scope.selectUserIndex = function (index) {
-      if ($scope.selectedUserIndex !== index) {
-        $scope.selectedUserIndex = index;
+      if ($scope.selectedTransactionIndex !== index) {
+        $scope.selectedTransactionIndex = index;
       }
       else {
-        $scope.selectedUserIndex = undefined;
+        $scope.selectedTransactionIndex = undefined;
       }
     };
 }]);
